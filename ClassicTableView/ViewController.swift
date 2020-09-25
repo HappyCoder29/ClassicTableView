7875fd2b9c23285d88c947ad8150add945329395
Ashish <drillmaps@gmail.com>
2020-09-24 20:07:22 -0700

    Initial Commit

---
+++ ClassicTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClassicTableView
+//
+//  Created by Ashish Ashish on 9/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
